cc7bc618c17accba6f86ecb2f1b70595fbf259c8
albertrocha031 <albertrocha031@gmail.com>
2024-02-27 13:33:22 -0500

    rendered individual tour on screen using map. here i worked on tours.jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
+import Loading from './components/Loading';
 import './App.css';
+import Tours from './components/Tours';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -12,10 +14,11 @@ function App() {
     try {
       const res = await fetch(url);
       const tours = await res.json();
-      console.log(tours)
+      setTours(tours)
     } catch (e) {
       console.log(e)
     }
+    setIsLoading(false)
   }
 
 
@@ -23,8 +26,20 @@ function App() {
     fetchTours()
   }, [])
 
+
+  if(isLoading) {
+    return (
+      <Loading />
+    )
+  }
+
   return (
-    <h1>Tours Project</h1>
+    <>
+      <h1>Tours Project</h1>
+      <main>
+        <Tours tours={tours} />
+      </main>
+    </>
   );
 }
 
